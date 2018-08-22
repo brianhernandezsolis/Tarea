1786c0b340051398491b36db5a7d797d18c75e70
brianhernandezsolis <42590485+brianhernandezsolis@users.noreply.github.com>
2018-08-21 18:10:18 -0600

    Add files via upload

---
+++ comprueba_mail/src/comprueba_mail/Comprueba_mail.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package comprueba_mail;
+
+/**
+ *
+ * @author Brian
+ */
+import javax.swing.*;
+public class Comprueba_mail {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        boolean arroba=false;
+        
+        String mail=JOptionPane.showInputDialog("Introduce Mail");
+        
+        for( int i=0; i<mail.length();i++);
+        
+    }
+    
+}
