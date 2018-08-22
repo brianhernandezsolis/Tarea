d7bb3248841973047424519e1f1082b81d1ccfee
brianhernandezsolis <42590485+brianhernandezsolis@users.noreply.github.com>
2018-08-21 18:16:10 -0600

    Add files via upload

---
+++ Declaraciones_Operadores/src/declaraciones_operadores/Declaraciones_Operadores.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package declaraciones_operadores;
+
+/**
+ *
+ * @author Brian
+ */
+public class Declaraciones_Operadores {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        int a=5;
+        
+        int b; 
+        
+        b=7;
+        
+        int c=b+a;
+        
+        c++; /* El resultante es 12, pero al poner 
+        ++ incrementa un numero
+        */
+        // c+=5; // aqui le decimos que aumente 5 numeros a la cantidad
+        
+        // c-=5; // aqui le decimos que decremente 5 numeros a la cantidad
+        
+        System.out.println(c);
+    }
+    
+}
